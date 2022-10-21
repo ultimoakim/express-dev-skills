5e2ae26c16bd29fed7c66b0edc4ce2b929128b8e
ultimoakim <akandykim234@gmail.com>
2022-10-21 11:33:46 -0700

    completed second part of the lab

--- models/skill.js
+++ models/skill.js
@@ -7,8 +7,21 @@ const skills = [
   
   module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne
   };
+  function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+  }
+
+  function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skills.push(skill);
+  }
   
   function getAll() {
     return skills; // This returns all to-do's, or in this case, all our skills.
--- routes/skills.js
+++ routes/skills.js
@@ -8,7 +8,14 @@ var skillsCtrl = require('../controllers/skills');
 
 // GET /skills
 router.get('/', skillsCtrl.index);
+// GET /skills/new 
+router.get('/new', skillsCtrl.new);
+// GET /skills/:id
 router.get('/:id', skillsCtrl.show);
+// POST /skills
+router.post('/', skillsCtrl.create); // add this route
+// new route below
+router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
 
